12347f4f56aa64fceae7f06ee8770d7d7a489222
Evgeny Artemev <artemev.15.01.1998@list.ru>
2021-03-22 09:30:24 +0300

    Using shape

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import numpy
-import cv2
+import numpy as np
+import cv2 as cv
 import pytesseract
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import matplotlib.pyplot as plt
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    image = cv.imread("photo15.jpg")
+    imageBGRGray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    imageRGBGray = cv.cvtColor(imageBGRGray, cv.COLOR_BGR2RGB)
+    shape = cv.imread("shape2.jpg")
+    shapeBGRGray = cv.cvtColor(shape, cv.COLOR_BGR2GRAY)
+    shapeRGBGray = cv.cvtColor(shapeBGRGray, cv.COLOR_BGR2RGB)
+    normimageRGBGray = cv.normalize(imageRGBGray, None, 0, 1, cv.NORM_MINMAX, cv.CV_32F)#попробовать разделить на макс mageRGBGRAY
+    normshapeRGBGray = cv.normalize(shapeRGBGray, None, 0, 1, cv.NORM_MINMAX, cv.CV_32F)
+    h, w, c = normshapeRGBGray.shape[::]
+    res = cv.matchTemplate(normimageRGBGray, normshapeRGBGray, cv.TM_CCOEFF_NORMED)
+    threshold = 0.1
+    loc = np.where(res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv.rectangle(normimageRGBGray, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
+    image = cv.cvtColor(normimageRGBGray, cv.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.show()
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
